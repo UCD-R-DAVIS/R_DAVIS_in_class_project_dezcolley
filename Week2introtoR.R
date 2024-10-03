250e96669878a9760cf13dd89c075da92dee837e
Desmond Colley <dezcolley@campus-112-179.ucdavis.edu>
2024-10-03 14:01:12 -0700

    Week 2 in class

---
+++ Week2introtoR.R
@@ -0,0 +1,98 @@
+3 + 4
+2 * 5
+#order of operations
+
+4 + 8 * 3 ^ 2
+4 + (8 * 3) ^ 2
+#this is a comment
+
+#scientific notation
+2 / 100000
+4e3
+
+#mathematical functions
+exp(1)
+log(1)
+log(4)
+sqrt(4)
+
+#r help files
+?log
+log(2, 4)
+log(4,2)
+log(x = 2, base = 4)
+
+#six comparison functions
+mynumber <- 6
+
+mynumber ==5
+#dont use one = sign
+
+mynumber != 5
+mynumber <- 5
+
+mynumber <3
+
+mynumber >= 2
+
+mynumber <-7
+
+othernumber <- 3
+
+#object and assignment
+mynumber * othernumber
+
+#object name conventions
+numSamples <- 50
+num_samples <- 40
+
+rm(num_samples)
+
+#tab completion
+ls()
+
+#remove variables
+rm(list = ls())
+
+#errors and warnings
+log_of_word <- log("a_word")
+log_of_word
+
+rm(mynumber)
+rm(numSamples)
+rm(othernumber)
+
+#warnings
+log_of_negative <- log(-2)
+
+
+
+#challenge
+elephant1_kg <- 3492
+elephant2_kg <- 7757
+
+elephant1_lb <- elephant1_kg * 2.2
+elephant2_lb > elephant1_lb
+
+rm(elephant2_lb)
+
+elephant2_lb > elephant1_lb
+
+myelephants <- c(elephant1_lb, elephant2_lb)
+
+
+#introduction to R: arithmatic
+
+
+
+
+
+
+
+
+
+
+
+
+
+
